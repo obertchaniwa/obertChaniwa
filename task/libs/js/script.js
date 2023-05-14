5ea414c03628e9983d137abce7f31a911bf7bfd1
Obert Chaniwa <116518966+obertchaniwa@users.noreply.github.com>
2023-05-14 13:56:16 +0100

    Update script.js

--- task/libs/js/script.js
+++ task/libs/js/script.js
@@ -1,12 +1,13 @@
-//FIRST API:
+//countryInfo:
 $('#btn1').click(function() {
 
     $.ajax({
-        url: "libs/php/temp1API.php",
-        type: 'POST/GET',
+        url: "libs/php/countryInfo.php",
+        type: 'POST',
         dataType: 'json',
         data: {
-
+            country: $('#selCountry').val(),
+            lang: $('#selLanguage').val()
         },
         success: function(result) {
 
@@ -14,32 +15,31 @@ $('#btn1').click(function() {
 
             if (result.status.name == "ok") {
 
-                $('#txtContent').html(result['data'][0]['content']);
-                $('#txtContent').html(result['data'][0]['content']);
-                $('#txtContent').html(result['data'][0]['content']);
-                $('#txtContent').html(result['data'][0]['content']);
-                $('#txtContent').html(result['data'][0]['content']);
-
+                $('#txtContinent').html(result['data'][0]['continent']);
+                $('#txtCapital').html(result['data'][0]['capital']);
+                $('#txtLanguages').html(result['data'][0]['languages']);
+                $('#txtPopulation').html(result['data'][0]['population']);
+                $('#txtArea').html(result['data'][0]['areaInSqKm']);
             }
         
         },
         error: function(jqXHR, textStatus, errorThrown) {
-            //  code error to go here
         }
     }); 
 
 });
 
 
-//SECOND API
+//oceans
 $('#btn2').click(function() {
 
     $.ajax({
-        url: "libs/php/temp2API.php",
-        type: 'POST/GET',
+        url: "libs/php/ocean.php",
+        type: 'POST',
         dataType: 'json',
         data: {
-
+            lat: $('#lat').val(),
+            lng: $('#lng').val(),
         },
         success: function(result) {
 
@@ -47,17 +47,12 @@ $('#btn2').click(function() {
 
             if (result.status.name == "ok") {
 
-                $('#txtContent').html(result['data'][0]['content']);
-                $('#txtContent').html(result['data'][0]['content']);
-                $('#txtContent').html(result['data'][0]['content']);
-                $('#txtContent').html(result['data'][0]['content']);
-                $('#txtContent').html(result['data'][0]['content']);
-
+                $('#txtName').html(result['data'][0]['name']);
+                $('#txtDistance').html(result['data'][0]['distance']);
             }
         
         },
         error: function(jqXHR, textStatus, errorThrown) {
-            //  code error to go here
         }
     }); 
 
@@ -65,14 +60,16 @@ $('#btn2').click(function() {
 
 
 
-//THIRD API
-$('#btn2').click(function() {
+//neighbours
+$('#btn3').click(function() {
 
     $.ajax({
-        url: "libs/php/temp3API.php",
-        type: 'POST/GET',
+        url: "libs/php/neighbours.php",
+        type: 'POST',
         dataType: 'json',
         data: {
+            endpoint: "neighbours",
+            country: $("#country").val(),
 
         },
         success: function(result) {
@@ -81,17 +78,11 @@ $('#btn2').click(function() {
 
             if (result.status.name == "ok") {
 
-                $('#txtContent').html(result['data'][0]['content']);
-                $('#txtContent').html(result['data'][0]['content']);
-                $('#txtContent').html(result['data'][0]['content']);
-                $('#txtContent').html(result['data'][0]['content']);
-                $('#txtContent').html(result['data'][0]['content']);
-
+                $('#txtCountry').html(result['data'][0]['country'])
             }
         
         },
         error: function(jqXHR, textStatus, errorThrown) {
-            //  code error to go here
         }
     }); 
 
