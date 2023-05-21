c3f102c6179fdb122892e2191275eab47f5fe550
Obert Chaniwa <116518966+obertchaniwa@users.noreply.github.com>
2023-05-21 19:08:43 +0100

    Update weather.php

--- task/libs/php/weather.php
+++ task/libs/php/weather.php
@@ -24,7 +24,7 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['weather'];
+	$output['data'] = $decode['weatherObservations'];
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
